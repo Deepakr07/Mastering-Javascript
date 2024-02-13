c5beff51ae2f2623f65e0774135b64b5e30c52c3
deepak <deepakrajesh567@gmail.com>
2024-02-13 19:28:53 +0530

    Amazon project --started learning basic mathematical operations and in build functions

--- math.js
+++ math.js
@@ -1,3 +1,8 @@
 //this is to learn js math operations
 let cost  = 10 + 3*(8)+5;
 console.log(cost);
+
+//round functions
+//Math.round function is used in javascript to round off a number.
+console.log(Math.round(3.5))
+console.log(Math.round(3.2))
