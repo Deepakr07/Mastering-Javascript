90e4b77d78c10035d258e7307dd9227c9bb8e3e4
deepak <deepakrajesh567@gmail.com>
2024-02-18 12:50:04 +0530

    revisiting js objects

--- practice.js
+++ practice.js
@@ -13,3 +13,14 @@ console.log(Math.round(3.2))
 let celcius = prompt("Enter the temperature in celcius")
 let fahrenheit = celcius*(9/5)+32;
 console.log("The temperature in fahrenheit is ",fahrenheit);
+
+//revisiting javaScript objects
+
+const profile = {
+    userName:"deepak",
+    isFollow : true,
+    followers : 750,
+    following : 700,
+    posts : 10
+
+};
